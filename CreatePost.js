3a61ea3825a22d652e510abbcd4cf1a509b497a4
AritroD7 <108117489+AritroD7@users.noreply.github.com>
2023-03-24 18:31:41 +0600

    Add files via upload

---
+++ CreatePost.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+    return (
+      
+        <Text>
+          CreatePost 
+        </Text>
+  
+    );
+  }
+  
